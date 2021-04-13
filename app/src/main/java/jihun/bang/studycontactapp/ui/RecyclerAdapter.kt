7bfc58549e3d2beda7d20fc45843490d07ed4c05
jihun.bang <uwlgnsu@naver.com>
2021-04-14 05:27:49 +0900

    [Adapter] 로그 추가
    [DESC]
    로그 추가

--- app/src/main/java/jihun/bang/studycontactapp/ui/RecyclerAdapter.kt
+++ app/src/main/java/jihun/bang/studycontactapp/ui/RecyclerAdapter.kt
@@ -10,22 +10,25 @@ import jihun.bang.studycontactapp.data.ContactsResponse
 
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerViewHolder>() {
     val modelList = mutableListOf<ContactModel>()
+
+    // 목록의 아이템 수
+    override fun getItemCount(): Int {
+        Log.d("로그", "[RecyclerAdapter][getItemCount] Called")
+        return modelList.size
+    }
+
     // View Holder 생성
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
         // 연결할 레이아웃 설정
         Log.d("로그", "[RecyclerAdapter][onCreateViewHolder] Called")
         return RecyclerViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
+                LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
         )
-    }
-
-    // 목록의 아이템 수
-    override fun getItemCount(): Int {
-        return modelList.size
     }
 
     // 뷰와 뷰홀더가 묶였을 때
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        Log.d("로그", "[RecyclerAdapter][onBindViewHolder] Called")
         holder.bind(modelList[position])
     }
 
